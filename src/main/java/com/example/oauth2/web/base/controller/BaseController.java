9df0de1dc9952bda233254ff4c814f405730a561
chengcheng <shmily_think@126.com>
2020-03-16 00:19:52 +0800

    查询client登录的用户信息

--- src/main/java/com/example/oauth2/web/base/controller/BaseController.java
+++ src/main/java/com/example/oauth2/web/base/controller/BaseController.java
@@ -1,5 +1,7 @@
 package com.example.oauth2.web.base.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,11 @@ public class BaseController {
     @RequestMapping()
     public String base() {
         return "This is base";
+    }
+
+    @RequestMapping("user")
+    public Authentication user() {
+        return SecurityContextHolder.getContext().getAuthentication();
     }
 
 }
